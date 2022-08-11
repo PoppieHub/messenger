9c0d3191688ad5d9f01a8d3fe92d1cbe20949a73
poppie <hoolm-2012@yandex.ru>
2022-08-11 07:25:57 +0500

    add MessageService.php

---
+++ code/src/Service/MessageService.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Message;
+use App\Exception\ChatNotFoundException;
+use App\Model\MessagesListItem;
+use App\Model\MessagesListResponse;
+use App\Repository\ChatRepository;
+use App\Repository\MessageRepository;
+
+class MessageService
+{
+    public function __construct(private ChatRepository $chatRepository, private MessageRepository $messageRepository)
+    {
+    }
+
+    public function getMessagesByChat(string $chatId): MessagesListResponse
+    {
+        $chat = $this->chatRepository->find($chatId);
+
+        if ($chat === null) {
+            throw new ChatNotFoundException();
+        }
+
+        return new MessagesListResponse(array_map(
+            [$this, 'map'],
+            $this->messageRepository->findMessagesByChatId($chatId)
+        ));
+    }
+
+    private function map(Message $message): MessagesListItem
+    {
+        return (new MessagesListItem())
+            ->setId($message->getId())
+            ->setBodyMessage($message->getBodyMessage())
+            ->setIsRead($message->isIsRead())
+            ->setCreatedAt($message->getCreatedAt()->getTimestamp())
+            ->setUpdatedAt(
+                ($message->getUpdatedAt())?$message->getUpdatedAt()->getTimestamp():$message->getUpdatedAt()
+            );
+    }
+}
