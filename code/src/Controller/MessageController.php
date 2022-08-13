c08e8548a792f5124aca215b17988948b8db9ca1
poppie <hoolm-2012@yandex.ru>
2022-08-14 00:06:32 +0500

    Added exception handling

--- code/src/Controller/MessageController.php
+++ code/src/Controller/MessageController.php
@@ -2,14 +2,13 @@
 
 namespace App\Controller;
 
-use App\Exception\ChatNotFoundException;
 use App\Service\MessageService;
 use OpenApi\Annotations as OA;
 use App\Model\MessagesListResponse;
+use App\Model\ErrorResponse;
 use Nelmio\ApiDocBundle\Annotation\Model;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/api/v1/chat/{id}/messages', name: 'messages.')]
@@ -25,14 +24,15 @@ class MessageController extends AbstractController
      *     description="Return messages by chat",
      *     @Model(type=MessagesListResponse::class)
      * )
+     * @OA\Response(
+     *     response=404,
+     *     description="Chat not found",
+     *     @Model(type=ErrorResponse::class)
+     * )
      */
     #[Route(path: '/', name: 'getMessages', methods: ['GET'])]
     public function messagesByChat(string $id): Response
     {
-        try {
-            return $this->json($this->messageService->getMessagesByChat($id));
-        } catch (ChatNotFoundException $exception) {
-            throw new HttpException($exception->getCode(), $exception->getMessage());
-        }
+        return $this->json($this->messageService->getMessagesByChat($id));
     }
 }
--- code/src/Exception/ChatNotFoundException.php
+++ code/src/Exception/ChatNotFoundException.php
@@ -3,12 +3,11 @@
 namespace App\Exception;
 
 use RuntimeException;
-use Symfony\Component\HttpFoundation\Response;
 
 class ChatNotFoundException extends RuntimeException
 {
     public function __construct()
     {
-        parent::__construct("Chat not found", Response::HTTP_NOT_FOUND);
+        parent::__construct("Chat not found");
     }
 }
