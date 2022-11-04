4fd6379fc06f04477ca1bf177ffd63fcdf5a574c
poppie <hoolm-2012@yandex.ru>
2022-11-04 05:29:48 +0500

    update fakeData

--- app/frontend/src/pages/Home/fakeData.ts
+++ app/frontend/src/pages/Home/fakeData.ts
@@ -2,107 +2,113 @@ import {MessagesListResponse} from "../../models/response/MessagesListResponse";
 import {UserListItem} from "../../models/response/UserListItem";
 import {ContentListResponse} from "../../models/response/ContentListResponse";
 import {MessagesListItem} from "../../models/response/MessagesListItem";
+import {ChatsListResponse} from "../../models/response/ChatsListResponse";
+import {ChatsListItem} from "../../models/response/ChatsListItem";
 
-export const fakeMessageListResponse = ():MessagesListResponse => {
-    let userFirst: UserListItem = {
-        id: '12',
-        email: 'test7@test.com',
-        nickname: 'testNicknameTest',
-        firstName: '',
-        lastName: 'TestLastName',
-        verified: true,
-        content: {
-            items: [
-                {id: '1', link: 'https://sun9-19.userapi.com/impg/z6uhkAgx2KhgretMc8a8YzxasZqxnpoR1xouEw/edMDxXBLi4Y.jpg?size=1241x932&quality=96&sign=9c9ce6c17bb3106fc647cbc1ca2c854a&type=album', avatar: true},
-                {id: '2', link: 'https://lelolobi.com/wp-content/uploads/2021/11/Test-Logo-Small-Black-transparent-1-1.png', avatar: true}
-            ]
+export const fakeUserFirst: UserListItem = {
+    id: '12',
+    email: 'test7@test.com',
+    nickname: 'testNicknameTest',
+    firstName: '',
+    lastName: 'TestLastName',
+    verified: true,
+    content: {
+        items: [
+            {id: '1', link: 'https://sun9-19.userapi.com/impg/z6uhkAgx2KhgretMc8a8YzxasZqxnpoR1xouEw/edMDxXBLi4Y.jpg?size=1241x932&quality=96&sign=9c9ce6c17bb3106fc647cbc1ca2c854a&type=album', avatar: true},
+            {id: '2', link: 'https://lelolobi.com/wp-content/uploads/2021/11/Test-Logo-Small-Black-transparent-1-1.png', avatar: true}
+        ]
+    },
+    hideEmail: false
+}
+
+export const fakeUserSecond: UserListItem = {
+    id: '13',
+    email: 'dvdv@dsfdsvd.fsf',
+    nickname: 'vdfvfdvfdvd',
+    firstName: 'TestSecondName',
+    lastName: 'TestSecondName',
+    verified: true,
+    content: {
+        items: [
+            {id: '1', link: 'https://lelolobi.com/wp-content/uploads/2021/11/Test-Logo-Small-Black-transparent-1-1.png', avatar: true},
+            {id: '2', link: 'https://sun9-19.userapi.com/impg/z6uhkAgx2KhgretMc8a8YzxasZqxnpoR1xouEw/edMDxXBLi4Y.jpg?size=1241x932&quality=96&sign=9c9ce6c17bb3106fc647cbc1ca2c854a&type=album', avatar: true}
+        ]
+    },
+    hideEmail: true
+};
+
+export const contentList: ContentListResponse = {
+    items: [
+        {
+            id:'4',
+            link: 'https://upload.wikimedia.org/wikipedia/en/9/96/Meme_Man_on_transparent_background.webp',
+            avatar: false
         },
-        hideEmail: false
-    };
-
-    let userSecond: UserListItem = {
-        id: '13',
-        email: 'dvdv@dsfdsvd.fsf',
-        nickname: 'vdfvfdvfdvd',
-        firstName: 'TestSecondName',
-        lastName: 'TestSecondName',
-        verified: true,
-        content: {
-            items: [
-                {id: '1', link: 'https://lelolobi.com/wp-content/uploads/2021/11/Test-Logo-Small-Black-transparent-1-1.png', avatar: true},
-                {id: '2', link: 'https://sun9-19.userapi.com/impg/z6uhkAgx2KhgretMc8a8YzxasZqxnpoR1xouEw/edMDxXBLi4Y.jpg?size=1241x932&quality=96&sign=9c9ce6c17bb3106fc647cbc1ca2c854a&type=album', avatar: true}
-            ]
+        {
+            id:'5',
+            link: 'https://i.pinimg.com/originals/68/a7/cd/68a7cd011d98c6e6599dfe3a769465e5.jpg',
+            avatar: false
         },
-        hideEmail: true
-    };
+        {
+            id:'6',
+            link: 'https://upload.wikimedia.org/wikipedia/ru/thumb/4/4d/Wojak.png/200px-Wojak.png',
+            avatar: false
+        },
+        {
+            id:'7',
+            link: 'https://upload.wikimedia.org/wikipedia/ru/thumb/4/4d/Wojak.png/200px-Wojak.gif',
+            avatar: false
+        }
+    ]
+};
 
-    let contentList: ContentListResponse = {
+export const firstMessage: MessagesListItem = {
+    id: '1',
+    user: fakeUserFirst,
+    read: {
+        items: []
+    },
+    reply: {
+        items: []
+    },
+    body_message: {
+        content: contentList,
+        message: 'Тестовое сообщение 1!',
+        replyMessage: {
+            items: []
+        }
+    },
+    created_at: new Date().getTime(),
+};
+
+export const secondMessage: MessagesListItem = {
+    id: '2',
+    user: fakeUserSecond,
+    read: {
         items: [
             {
-                id:'4',
-                link: 'https://upload.wikimedia.org/wikipedia/en/9/96/Meme_Man_on_transparent_background.webp'
-            },
-            {
-                id:'5',
-                link: 'https://i.pinimg.com/originals/68/a7/cd/68a7cd011d98c6e6599dfe3a769465e5.jpg'
-            },
-            {
-                id:'6',
-                link: 'https://upload.wikimedia.org/wikipedia/ru/thumb/4/4d/Wojak.png/200px-Wojak.png'
-            },
-            {
-                id:'7',
-                link: 'https://upload.wikimedia.org/wikipedia/ru/thumb/4/4d/Wojak.png/200px-Wojak.gif'
+                id: '1',
+                user: fakeUserFirst
             }
         ]
-    };
-
-    let firstMessage: MessagesListItem = {
-        id: '1',
-        user: userFirst,
-        read: {
-            items: []
-        },
-        reply: {
-            items: []
-        },
-        body_message: {
-            content: contentList,
-            message: 'Тестовое сообщение 1!',
-            replyMessage: {
-                items: []
-            }
-        },
-        created_at: new Date().getTime(),
-    };
-
-    let secondMessage: MessagesListItem = {
-        id: '2',
-        user: userSecond,
-        read: {
+    },
+    reply: {
+        items: [firstMessage]
+    },
+    body_message: {
+        content: contentList,
+        message: 'Тестовое сообщение 2!',
+        replyMessage: {
             items: [
-                {
-                    id: '1',
-                    user: userFirst
-                }
+                {id: firstMessage.id}
             ]
-        },
-        reply: {
-            items: [firstMessage]
-        },
-        body_message: {
-            content: contentList,
-            message: 'Тестовое сообщение 2!',
-            replyMessage: {
-                items: [
-                    {id: firstMessage.id}
-                ]
-            }
-        },
-        created_at: new Date().getTime()-1000000,
-        updated_at: new Date().getTime()-500000,
-    };
+        }
+    },
+    created_at: new Date().getTime()-1000000,
+    updated_at: new Date().getTime()-500000,
+};
 
+export const fakeMessageListResponse = ():MessagesListResponse => {
     return (
         {
             items: [
@@ -111,6 +117,52 @@ export const fakeMessageListResponse = ():MessagesListResponse => {
             ]
         }
     );
-}
+};
 
-export default fakeMessageListResponse();
+export const firstFakeChat: ChatsListItem = {
+    id: '1',
+    name: 'First Test Chat',
+    description: 'Fake description',
+    multiChat: false,
+    content: {
+        items: [
+        ]
+    },
+    membership: {
+        items: [
+            {id: '1', notification: false, usersListItem: fakeUserFirst},
+            {id: '2', notification: false, usersListItem: fakeUserSecond}
+        ]
+    },
+    messages: fakeMessageListResponse(),
+    lastMessage: secondMessage,
+    unreadMessageCounter: 1
+};
+
+export const secondFakeChat: ChatsListItem = {
+    id: '2',
+    name: 'Second Test Chat',
+    description: 'Fake description - second',
+    multiChat: true,
+    content: {
+        items: [
+            {id: '1', link: 'https://lelolobi.com/wp-content/uploads/2021/11/Test-Logo-Small-Black-transparent-1-1.png', avatar: true},
+            {id: '2', link: 'https://sun9-19.userapi.com/impg/z6uhkAgx2KhgretMc8a8YzxasZqxnpoR1xouEw/edMDxXBLi4Y.jpg?size=1241x932&quality=96&sign=9c9ce6c17bb3106fc647cbc1ca2c854a&type=album', avatar: true}
+        ]
+    },
+    membership: {
+        items: [
+            {id: '1', notification: false, usersListItem: fakeUserSecond},
+            {id: '2', notification: false, usersListItem: fakeUserFirst}
+        ]
+    },
+    messages: fakeMessageListResponse(),
+    lastMessage: firstMessage,
+    unreadMessageCounter: 0
+};
+
+export const fakeChatsListResponse = ():ChatsListResponse => {
+    return (
+        {items: [firstFakeChat, secondFakeChat]}
+    );
+}
