fafa391fb4c28c6cf7476062039029003f58398c
poppie <hoolm-2012@yandex.ru>
2022-11-05 02:59:22 +0500

    Update Message, added reply-render

--- app/frontend/src/components/Message/index.tsx
+++ app/frontend/src/components/Message/index.tsx
@@ -12,9 +12,10 @@ import './Message.scss';
 
 interface MessageProps {
     message: MessagesListItem;
+    replyStatus?: boolean;
 }
 
-const Message: React.FC<MessageProps> = (props) => {
+const Message: React.FC<MessageProps> = ({message, replyStatus = false}) => {
     const {store} = React.useContext(Context);
 
     const checkRead = (read: ReadMessageListResponse): boolean => {
@@ -27,56 +28,70 @@ const Message: React.FC<MessageProps> = (props) => {
 
     return (
         <div className={classNames('message',
-            {'message--isme':isMe(props.message.user, store)})}>
-            <div className='message__content'>
-                <div className={classNames({
-                    'message__icon-read': isMe(props.message.user, store),
-                    'message__icon--doubleTicks': isMe(props.message.user, store) && props.message.read && checkRead(props.message.read),
-                    'message__icon--ticks': isMe(props.message.user, store) && props.message.read && !checkRead(props.message.read)
+            {
+                'message--isme': isMe(message.user, store) && !replyStatus,
+                'message--reply': replyStatus
+            })}>
+            <div className={classNames('message__content',
+                {'message__content--borderLine': replyStatus})}>
+                {!replyStatus &&
+                    <div className={classNames({
+                            'message__icon-read': isMe(message.user, store),
+                            'message__icon--doubleTicks': isMe(message.user, store) && message.read && checkRead(message.read),
+                            'message__icon--ticks': isMe(message.user, store) && message.read && !checkRead(message.read)
+                        }
+                    )}/>
                 }
-                )} />
-                <div className='message__avatar'>
-                    {props.message.user.content && props.message.user.content.items[0].link &&
-                        <img src={props.message.user.content.items[0].link} alt={props.message.user.nickname}/>
-                    }
-                </div>
+                {!replyStatus &&
+                    <div className='message__avatar'>
+                        {message.user.content && message.user.content.items[0].link &&
+                            <img src={message.user.content.items[0].link} alt={message.user.nickname}/>
+                        }
+                    </div>
+                }
                 <div className='message__info'>
                     <div className='message__name'>
-                        {(props.message.user.firstName)
-                            ?<p className='message__name--normalName'>{props.message.user.firstName}</p>
-                            :<p className='message__name--nickname'>{props.message.user.nickname}</p>
+                        {(message.user.firstName)
+                            ?<p className='message__name--normalName'>{message.user.firstName}</p>
+                            :<p className='message__name--nickname'>{message.user.nickname}</p>
                         }
                     </div>
                     {
-                        ((props.message.body_message &&
-                            props.message.body_message.message &&
-                            props.message.body_message.message.length !== 0) ||
-                            (props.message.reply && props.message.reply.items && props.message.reply.items.length !== 0)) &&
+                        ((message.body_message &&
+                            message.body_message.message &&
+                            message.body_message.message.length !== 0) ||
+                            (message.reply && message.reply.items && message.reply.items.length !== 0)) &&
                         <div className='message__bubble'>
-                            {props.message.body_message && props.message.body_message?.message &&
-                                <p className='message__text'>{props.message.body_message?.message}</p>
+                            {message.body_message && message.body_message?.message &&
+                                <p className='message__text'>{message.body_message?.message}</p>
+                            }
+                            {
+                                message.reply && message.reply.items && message.reply.items.length > 0 &&
+                                message.reply.items.map((item)=>
+                                    <Message key={item.id} message={item} replyStatus={true} />
+                                )
                             }
                         </div>
                     }
                     <div className='message__attachments'>
                         {
-                            props.message.body_message &&
-                            props.message.body_message.content &&
-                            props.message.body_message.content.items &&
-                            props.message.body_message.content.items.length > 0 &&
-                            props.message.body_message.content.items.length !== 1 &&
-                            props.message.body_message.content.items.map((item) =>
+                            message.body_message &&
+                            message.body_message.content &&
+                            message.body_message.content.items &&
+                            message.body_message.content.items.length > 0 &&
+                            message.body_message.content.items.length !== 1 &&
+                            message.body_message.content.items.map((item) =>
                                 <div className='message__attachments-item' key={item.id}>
                                     {(item.link && mime.getType(item.link)?.includes('image')) &&
                                         <img src={item.link} alt={item.id?item.id:'attachments ' + item.link} />}
                                 </div>
                             ) || (
-                                props.message.body_message &&
-                                props.message.body_message.content &&
-                                props.message.body_message.content.items &&
-                                props.message.body_message.content.items.length > 0 &&
-                                props.message.body_message.content.items.length === 1 &&
-                                props.message.body_message.content.items.map((item) =>
+                                message.body_message &&
+                                message.body_message.content &&
+                                message.body_message.content.items &&
+                                message.body_message.content.items.length > 0 &&
+                                message.body_message.content.items.length === 1 &&
+                                message.body_message.content.items.map((item) =>
                                     <div className='message__attachments-item--onlyOneItem' key={item.id}>
                                         {(item.link && mime.getType(item.link)?.includes('image')) &&
                                             <img src={item.link} alt={item.id?item.id:'attachments ' + item.link} />}
@@ -86,14 +101,14 @@ const Message: React.FC<MessageProps> = (props) => {
                         }
                     </div>
                     <span className='message__date'>
-                        {props.message.updated_at &&
+                        {message.updated_at &&
                             <p className='message__date--updateData'>
-                                {`обновлено ` + formatDistanceToNow(props.message.updated_at, {addSuffix: true, locale: ruLocale})}
+                                {`обновлено ` + formatDistanceToNow(message.updated_at, {addSuffix: true, locale: ruLocale})}
                             </p>
                         }
-                        {!props.message.updated_at && props.message.created_at &&
+                        {!message.updated_at && message.created_at &&
                             <p className='message__date--createData'>
-                                {`отправлено ` + formatDistanceToNow(props.message.created_at, {addSuffix: true, locale: ruLocale})}
+                                {`отправлено ` + formatDistanceToNow(message.created_at, {addSuffix: true, locale: ruLocale})}
                             </p>
                         }
                     </span>
--- app/frontend/src/pages/Home/index.tsx
+++ app/frontend/src/pages/Home/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import {Context} from "../../index";
 import {fakeMessageListResponse, fakeChatsListResponse} from './fakeData';
+import {Message} from "../../components";
 
 const Home = () => {
     const {store} = React.useContext(Context);
 
     return (
         <section className="home">
-
+            {fakeMessageListResponse().items.map((item)=>
+                <Message key={item.id} message={item} />
+            )}
         </section>
     );
 }
